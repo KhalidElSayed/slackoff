1b5a6fced4109ab4db3f2eb94eebe1df7919737d
Jake Klinker <jklinker1@gmail.com>
2013-12-02 11:23:00 -0600

    add layout for note activity

--- src/com/klinker/android/slackoff/ui/NoteActivity.java
+++ src/com/klinker/android/slackoff/ui/NoteActivity.java
@@ -1,7 +1,8 @@
 package com.klinker.android.slackoff.ui;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import com.klinker.android.slackoff.R;
 
 /**
  * Activity to handle all notes that are opened in the app (have an extension of .klink and this activity is registered
@@ -9,10 +10,11 @@ import android.os.Bundle;
  *
  * @author Jake and Luke Klinker
  */
-public class NoteActivity extends Activity {
+public class NoteActivity extends ListActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.note_activity);
     }
 }
